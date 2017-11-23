01045c3d88f157fc85bbe7dbac073a09eb9edd7d
Jean <jcjeancarlo@MacBook-Pro-de-ALOC-2.local>
2017-11-23 21:24:04 -0200

    Aula3

--- Calculator/CalculatorManager.swift
+++ Calculator/CalculatorManager.swift
@@ -3,13 +3,41 @@
 //  Calculator
 //
 //  Created by Jean Ferreira on 22/11/17.
-//  Copyright � 2017 jean rights reserved.
+//  Copyright � 2017 jean rights reserved.
 //
 import Foundation
 
 struct CalculatorManager {
-    private var accumulator: Double?
     
+    enum Operation {
+        case unaryOperation ((Double)->Double)
+        case binaryOperation ((Double,Double)->Double)
+        case equals
+        case unknown
+    }
+    
+    private var binaryOperationMemory:PreviousBinaryOperation?
+    
+    private struct PreviousBinaryOperation {
+        let function : (Double,Double)->Double
+        let firstOperand: (Double)
+        
+        func perform(with secondOperand:Double)->Double {
+            return function(firstOperand,secondOperand)
+        }
+    }
+    
+    private var accumulator: Double = 0.0
+    private let operations = [
+        "+":Operation.binaryOperation({$0 + $1}),
+        "-":Operation.binaryOperation({$0 - $1}),
+        "x":Operation.binaryOperation({$0 * $1}),
+        "÷":Operation.binaryOperation({$0 / $1}),
+        "±":Operation.unaryOperation({-$0}),
+        "%":Operation.unaryOperation({$0 / 100}),
+        "√":Operation.unaryOperation(sqrt),
+        "=":Operation.equals
+    ]
     
     var result: Double? {
         get {
@@ -17,11 +45,36 @@ struct CalculatorManager {
         }
     }
     
-    mutating func performOperation(_ symbol: String) {
-
+    //add
+    mutating func toucheClear(){
+        self.binaryOperationMemory = nil
     }
     
-    mutating func setOperand(_ operand: Double) {
+    mutating func doPreviousByOperation(){
+        guard let memory = binaryOperationMemory else { return }
+        accumulator = memory.perform(with: accumulator)
+        self.binaryOperationMemory = nil
+    }
+    
+    
+    mutating func performOperation(_ symbol: String) {
+        
+        guard let operation = operations[symbol] else {return}
+        
+        switch operation {
+        case .unaryOperation(let op):
+            accumulator = op(accumulator)
+        case .binaryOperation(let op):
+            binaryOperationMemory = PreviousBinaryOperation(function: op, firstOperand: accumulator)
+        case .equals:
+            doPreviousByOperation()
+        default:
+            break
+        }
+    }
+    
+    mutating func setOperand(_ operand: Double){
         accumulator = operand
     }
+    
 }
--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   @IBOutlet weak var display: UILabel!
+    private var manager = CalculatorManager()
+    @IBOutlet weak var display: UILabel!
     var userIsTyping: Bool = false
     
     var displayValue: Double {
@@ -31,7 +32,7 @@ class ViewController: UIViewController {
         let digit = sender.currentTitle!
         if userIsTyping {
             let textDisplay = display.text ?? ""
-            display.text = textDisplay + digit
+            display.text = textDisplay + digit //(digit == "-" && textDisplay == "0" ? "0" : digit)
         } else {
             display.text = digit
             userIsTyping = true
@@ -39,47 +40,20 @@ class ViewController: UIViewController {
     }
     
     @IBAction func performOperation(_ sender: UIButton) {
-        userIsTyping = false
-
-        if let mathSymbol = sender.currentTitle {
-            switch mathSymbol {
-            case "+":
-                break;
-            case "-":
-                break;
-            case "⨉":
-                break;
-            case "÷":
-                break;
-            case "=":
-                break;
-            case "±":
-                display.text = String(-displayValue)
-                break;
-            case "%":
-                break;
-            case "√":
-                display.text = String(sqrt(displayValue))
-                break;
-            default:
-                break;
-            }
-        }
-    }
-
-    @IBAction func performOperation(_ sender: UIButton) {
         if userIsTyping {
             userIsTyping = false
+            
             manager.setOperand(displayValue)
         }
-
-        if let mathSymbol = sender.currentTitle {
-            manager.performOperation(mathSymbol)
+        
+        if let matchSymbol = sender.currentTitle{
+            manager.performOperation(matchSymbol)
         }
         
-        if let result = manager.result {
+        if let result  = manager.result {
             displayValue = result
         }
+    
     }
 
 }
